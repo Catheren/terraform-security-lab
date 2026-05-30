7d8e530bd0e83364c510ffd411a5d75808f1aec5
Catheren Joseph <jcatheren@gmail.com>
2026-05-30 20:06:37 -0300

    Format Terraform files

--- main.tf
+++ main.tf
@@ -20,21 +20,21 @@ module "iam" {
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_name   = "security-lab-vpc"
-  vpc_cidr   = "10.0.0.0/16"
+  vpc_name    = "security-lab-vpc"
+  vpc_cidr    = "10.0.0.0/16"
   subnet_cidr = "10.0.1.0/24"
 }
 
 module "ec2" {
   source = "./modules/ec2"
 
-  ami_id                = "ami-0c02fb55956c7d316"
-  instance_type         = "t2.micro"
-  instance_name         = "security-lab-instance"
+  ami_id        = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  instance_name = "security-lab-instance"
 
-  subnet_id             = module.vpc.subnet_id
+  subnet_id = module.vpc.subnet_id
 
   instance_profile_name = module.iam.instance_profile_name
 
-  environment           = var.environment
+  environment = var.environment
 }
